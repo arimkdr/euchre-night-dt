2416e6d842e97ff457476de7a6523d89a6f1a635
amkdr <korotkin.ari@gmail.com>
2020-08-23 19:49:52 -0500

    added uuid to Player model

--- server/db/models/player.js
+++ server/db/models/player.js
@@ -2,21 +2,29 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Player = db.define('player', {
-    username: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: true
-        }
-    },
-    gamesWon: {
-        type: Sequelize.INTEGER,
-        defaultValue: 0
-    },
-    gamesLost: {
-        type: Sequelize.INTEGER,
-        defaultValue: 0
+  uuid: {
+    type: Sequelize.UUID,
+    primaryKey: true,
+    allowNull: false,
+    validate: {
+      notEmpty: true
     }
+  },
+  username: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  gamesWon: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0
+  },
+  gamesLost: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0
+  }
 })
 
 module.exports = Player
