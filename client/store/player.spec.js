e54d482f06e07812768718769be53d7452805f2b
amkdr <korotkin.ari@gmail.com>
2020-09-09 18:58:21 -0500

    fixed most test files to reflect changes from user => player

--- client/store/player.spec.js
+++ client/store/player.spec.js
@@ -1,7 +1,7 @@
 /* global describe beforeEach afterEach it */
 
 import {expect} from 'chai'
-import {me, logout} from './user'
+import {me, logout} from './player'
 import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
 import configureMockStore from 'redux-mock-store'
@@ -15,7 +15,7 @@ describe('thunk creators', () => {
   let store
   let mockAxios
 
-  const initialState = {user: {}}
+  const initialState = {player: {}}
 
   beforeEach(() => {
     mockAxios = new MockAdapter(axios)
@@ -28,22 +28,22 @@ describe('thunk creators', () => {
   })
 
   describe('me', () => {
-    it('eventually dispatches the GET USER action', async () => {
-      const fakeUser = {email: 'Cody'}
-      mockAxios.onGet('/auth/me').replyOnce(200, fakeUser)
+    it('eventually dispatches the GET PLAYER action', async () => {
+      const fakePlayer = {username: 'Cody'}
+      mockAxios.onGet('/auth/me').replyOnce(200, fakePlayer)
       await store.dispatch(me())
       const actions = store.getActions()
-      expect(actions[0].type).to.be.equal('GET_USER')
-      expect(actions[0].user).to.be.deep.equal(fakeUser)
+      expect(actions[0].type).to.be.equal('GET_PLAYER')
+      expect(actions[0].player).to.be.deep.equal(fakePlayer)
     })
   })
 
   describe('logout', () => {
-    it('logout: eventually dispatches the REMOVE_USER action', async () => {
+    it('logout: eventually dispatches the REMOVE_PLAYER action', async () => {
       mockAxios.onPost('/auth/logout').replyOnce(204)
       await store.dispatch(logout())
       const actions = store.getActions()
-      expect(actions[0].type).to.be.equal('REMOVE_USER')
+      expect(actions[0].type).to.be.equal('REMOVE_PLAYER')
       expect(history.location.pathname).to.be.equal('/login')
     })
   })
--- server/api/players.spec.js
+++ server/api/players.spec.js
@@ -4,29 +4,30 @@ const {expect} = require('chai')
 const request = require('supertest')
 const db = require('../db')
 const app = require('../index')
-const User = db.model('user')
+const Player = db.model('player')
 
-describe('User routes', () => {
+describe('Player routes', () => {
   beforeEach(() => {
     return db.sync({force: true})
   })
 
-  describe('/api/users/', () => {
-    const codysEmail = 'cody@puppybook.com'
+  describe('/api/players/', () => {
+    const testPlayer = 'test-player'
 
     beforeEach(() => {
-      return User.create({
-        email: codysEmail
+      return Player.create({
+        username: 'test-player',
+        password: 'test_password'
       })
     })
 
-    it('GET /api/users', async () => {
+    it('GET /api/players', async () => {
       const res = await request(app)
-        .get('/api/users')
+        .get('/api/players')
         .expect(200)
 
       expect(res.body).to.be.an('array')
-      expect(res.body[0].email).to.be.equal(codysEmail)
+      expect(res.body[0].username).to.be.equal(testPlayer)
     })
   }) // end describe('/api/users')
 }) // end describe('User routes')
