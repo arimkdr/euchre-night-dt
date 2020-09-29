2db664d326b62af8f5adae0642f36f856efe6c17
amkdr <korotkin.ari@gmail.com>
2020-09-28 20:57:51 -0500

    added more model tests

--- server/db/models.spec.js
+++ server/db/models.spec.js
@@ -19,9 +19,48 @@ describe('Player model', () => {
     }
   })
   after(() => db.sync({force: true}))
-  it.only('Allows a player to be added with a username and protected password', async () => {
+
+  it('Allows a player to be created with a username and protected password', async () => {
     const createdPlayer = await Player.create(testPlayer)
     expect(createdPlayer.username).to.equal('arimkdr')
     expect(createdPlayer.password()).to.not.equal('supersafe-password')
+  })
+})
+
+describe('Team model', () => {
+  let testPlayer1
+  let testPlayer2
+  let testTeam
+  before(() => db.sync({force: true}))
+  beforeEach(() => {
+    testPlayer1 = {
+      username: 'arimkdr',
+      password: 'supersafe-password'
+    }
+    testPlayer2 = {
+      username: 'dalinim',
+      password: 'unhackable-password'
+    }
+    testTeam = {
+      name: 'Habibis'
+    }
+  })
+  after(() => db.sync({force: true}))
+
+  it('Allows a team to be created with a name', async () => {
+    const createdTeam = await Team.create(testTeam)
+    expect(createdTeam.name).to.equal('Habibis')
+  })
+
+  it('Allows players to be added to a team', async () => {
+    const createdTeam = await Team.create(testTeam)
+    const createdPlayer1 = await Player.create(testPlayer1)
+    const createdPlayer2 = await Player.create(testPlayer2)
+    await createdTeam.addPlayer(createdPlayer1)
+    await createdTeam.addPlayer(createdPlayer2)
+    const teamPlayers = await createdTeam.getPlayers()
+    expect(teamPlayers).to.have.lengthOf(2)
+    expect(teamPlayers.map(player => player.username)).to.include('arimkdr')
+    expect(teamPlayers.map(player => player.username)).to.include('dalinim')
   })
 })
